d46106a15a8f64e8109eb9150c575065ff9904fe
Serge Koba <s.koba@mobidev.biz>
2018-01-13 21:35:59 +0200

    optimization

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose run trigger-service bundle
-docker-compose run trigger-service ./cli db_migrate
+docker-compose run trigger-service bundle exec ./cli db_migrate
 docker-compose run trigger-service bundle exec ./cli db_seed
 cd ../trigger-service/docs && mkdocs build # build api doc
